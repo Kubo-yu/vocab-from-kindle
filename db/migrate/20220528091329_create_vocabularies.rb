d0c0be91fb0f1d92b944a7c73223a60ed3f42023
Kubo-yu <kubomoto.y@cvcf.co.jp>
2022-05-29 17:46:44 +0900

    bookとvocabテーブル追加

--- db/migrate/20220528091329_create_vocabularies.rb
+++ db/migrate/20220528091329_create_vocabularies.rb
@@ -1,7 +1,12 @@
 class CreateVocabularies < ActiveRecord::Migration[6.1]
   def change
     create_table :vocabularies do |t|
-
+      t.belongs_to :book
+      t.string :word
+      t.string :phonics
+      t.text :definition
+      t.text :example
+      t.datetime :obtained_at
       t.timestamps
     end
   end
