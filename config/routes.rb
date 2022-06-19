673fc4d0be6617d64b53246180cff8ffb1e3a0e8
Kubo-yu <kubomoto.y@cvcf.co.jp>
2022-06-19 22:46:18 +0900

    スクレイピング 修正

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,5 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :scrapings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
